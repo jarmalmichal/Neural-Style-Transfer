c7edde29660840ea7d2977d27863a39525fbd596
michaljarmal <michal.m.jarmal@gmail.com>
2024-11-07 14:52:46 +0100

    Modify images

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -1,5 +1,5 @@
 import torch
-from torch.optim import Adam
+from torch.optim import Adam, LBFGS
 import os
 import argparse
 
@@ -28,6 +28,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--tv_weight", type=float, default=1e-1, help="weight for total variation loss"
     )
+    parser.add_argument("--optimizer", type=str, choices=["lbfgs", "adam"], default="adam")
     parser.add_argument("--lr", type=float, default=0.01, help="learning rate")
     parser.add_argument("--steps", type=int, default=3000, help="number of iterations")
 
@@ -63,7 +64,10 @@ if __name__ == "__main__":
 
     model = models.load_model(args.model).to(device)
 
-    optimizer = Adam([target_img], lr=args.lr)
+    if args.optimizer == "adam":
+        optimizer = Adam([target_img], lr=args.lr)
+    else:
+        optimizer = LBFGS([target_img], max_iter=20, line_search_fn='strong_wolfe')
 
     print(f"Running on {device} device")
     stylized_img = style_transfer(
